e00f73e56a7808c588beb6c30e0c013c8e312f70
Jonathan Kim <jkimbo@gmail.com>
2011-10-17 18:54:53 +0100

    Added db.local.php (ignoring it for the git repo) and added check in db.php

--- db.php
+++ db.php
@@ -1,7 +1,12 @@
 <?
-$dbname = 'scc_finance';
-$user = 'scc_finance';
-$pass = 'phqxNcL6BLSfLBjr';
+if(file_exists(getcwd().'/db.local.php')) {
+    require_once('db.local.php'); 
+} else {
+    $dbname = 'scc_finance';
+    $user = 'scc_finance';
+    $pass = 'phqxNcL6BLSfLBjr';
+}
+
 $connect = mysql_connect('localhost', $user, $pass);
 $sql = $connect;
 if (!$connect) {
