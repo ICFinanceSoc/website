7170c3d3355d505fd9486d38d871266939625693
Jonathan Kim <jkimbo@gmail.com>
2011-10-26 16:38:07 +0100

    Some small modifications to thumb boxes and sponsors

--- js/script.js
+++ js/script.js
@@ -11,11 +11,22 @@ $(document).ready(function() {
 
     if($('.boxthumb').length) {
         $('.boxthumb').hover(function() {
-            $(this).find('img').fadeTo('300', 0.2);
-            $(this).find('.caption').fadeIn('300');
+            var boxthumb = $(this);
+            var image = boxthumb.find('img');
+            var caption = boxthumb.find('.caption');
+            if (!boxthumb.hasClass('animated')) {
+                image.dequeue().stop().fadeTo('300', 0.2);
+                caption.dequeue().stop().fadeIn('300');
+            }
         }, function() {
-            $(this).find('img').fadeTo('300', 1);
-            $(this).find('.caption').fadeOut('300');
+            var boxthumb = $(this);
+            var image = boxthumb.find('img');
+            var caption = boxthumb.find('.caption');
+            boxthumb.addClass('animated');
+            caption.fadeOut('300');
+            image.fadeTo('300', 1, function() {
+                boxthumb.removeClass('animated').dequeue();
+            });
         });
     }
 });
