54ba2a378deeb471725a67a093eeb740fca3682b
Jonathan Kim <jkimbo@gmail.com>
2011-10-26 14:25:28 +0100

    about.php, index.php and subsidiaries.php now load from database

--- about.php
+++ about.php
@@ -1,61 +1,18 @@
+
 <?php
 $currentpage = 'about';
-require_once('header.php');?>
+require_once('header.php');
+mysql_select_db($database_sql, $sql);
+$query_homeregion = "SELECT * FROM homepage WHERE ID = 1";
+$homeregion = mysql_query($query_homeregion, $sql) or die(mysql_error());
+$row_homeregion = mysql_fetch_assoc($homeregion);
+$totalRows_homeregion = mysql_num_rows($homeregion);
+?>
 
-<h1>About</h1>
-
-<div id="icfs">
-    <h3>ICFS</h3>
-    <p class=displaytext>The Imperial College  Finance Society (ICFS) is the premier society for students looking for city  careers. With over 2400 members, we are the largest and most successful careers  focused society on campus. The results alone can be seen in the achievements of  the committee, with all those that have applied to date, achieving spring week  placements or internships, as well as numerous bursaries. The result of so many  members going on to get the city jobs that they have sought, is the growth of  an extensive network of alumni still involved with the society today. This  offers invaluable advice and experience to new members to help them land their  dream job. </p>
-    <p>We hold regular interactive  events with leading Investment Banks and City firms. These events are aimed at  providing our members with information about job opportunities in the City, as  well as offering a forum to network with leading professionals and bright  individuals on campus.</p>
-    <p>We also organise talks  and presentations on various financial topics such as derivatives, the recent  global financial crisis, emerging markets etc. These are given by prominent  speakers, who are experts in their respective fields.</p>
-    <p>In addition to this we  are building relationships with other academic institutions worldwide, to share  ideas and collectively pool knowledge and resources. In 2007-2008 ICFS  initiated the UK’s very first student led Inter-University Trading Game (IUTG)  which saw about 500 students from Imperial College, Oxford, Cambridge, Warwick  and LSE take part. The IUTG has now become an annual event sponsored by RBS. With  2010 bringing about the first year of the UK Investment banking series (<a href="http://www.UIBS.co.uk">www.UIBS.co.uk</a>), if you fancy yourself  as a trader or a banker, there has never been a better time to join.</p>
-    <p>Another great opportunity  available to members is the ICU Investment  Club. Following the impressive developments over the last year, this will  undoubtedly provide an excellent platform to learn and discuss the  opportunities of investing. Utilizing first-hand experience as the best way to  learn about the markets and understand the rationale of making stock choices. </p>
-    <p>ICFS is known by all the  major recruiters in the ABC sectors, and mentioning you have taken the effort  to participate in these events could transfer your application from the 'maybe'  to the 'yes' pile. The sooner you get involved in our activities the better, so  all you first and second years out there, JOIN UP (get involved) and make sure  you get clued up about the job you want.</p>
-</div>
-
-<div id="mission">
-    <h3>Mission Statement:</h3>
-    <p class=displaytext>We aim, through a number  of different initiatives and events, to provide our members with abundant  resources to enable them to learn as much about careers in finance and business  as possible. We aim to achieve this by:</p>
-    <p>Providing a platform for Imperial College Students interested in finance/business to learn more about  the industry, interact with professionals and foster relationships with a wide  network of individuals. </p>
-    <p>Providing a vibrant and  caring community that brings out the best in our members by encouraging and  rewarding initiative, creative thinking, professionalism and active  contribution. </p>
-</div>
-                
-<div id="corevalues">
-    <h3>Core Values:</h3>
-    <p>As a society we look to  help our members to grow as individuals, developing certain core values that we  want to carry into our chosen profession, including: integrity, leadership,  team work, professionalism and responsibility. We aim to provide an environment  in which members can develop these and other traits, which are key attributes  of successful business leaders.</p>
-</div>
-    
-<div id="tosponsors">
-    <h3>To our Sponsors:</h3>
-	<p class=displaytext>ICFS will offer a  reliable and professional service, utilizing the money invested efficiently to  offer the best value to sponsors. The society will raise the on campus profile of  our sponsors through a broad scope of marketing methods, utilizing strong  on-campus links established over time.</p>
-    <p>ICFS is proficient at  organising and promoting events even at short-notice, and communicating  information to students promptly, due to our efficient structure comprising of: </p>
-</div>
-
-<div id="sixmajor">
-    <h3>Six major divisions:</h3>
-    <ul>
-        <li><strong>Corporate Relations -</strong> Responsible for managing  the relationships between ICFS and its current sponsors.</li>
-        <li><strong>External Partnership -</strong> Mainly responsible for  establishing new partnerships and sponsorships within relevant industries.</li>
-        <li><strong>Marketing Division -</strong> focuses on advertising  events and promoting the society and Sponsors’ profiles on campus.</li>
-        <li><strong>Event Division -</strong> Oversees the planning and  smooth running of the society’s events on campus.</li>
-        <li><strong>Editorial Division -</strong> Capital Magazine –  covering insights and key events in the financial world, analysis on markets  and employer profiles of sponsoring companies.</li>
-        <li><strong>Logistics and Technology -</strong> Mainly operates to  maintain the ICFS website and provide technical support.</li>
-    </ul>
-</div>
-
-<div id="threeaddition">
-    <h3>Three additional branches:</h3>
-    <ul>
-        <li><strong>Investment Club Branch -</strong> A platform for the  students of Imperial College and Imperial College Business School to learn about  the financial markets, discuss trading opportunities and gain hands-on  experience in investment portfolio management. Many guest lectures have been  arranged to further enrich students’ understanding of trading in the real  world.</li>
-        <li><strong>Entrepreneurs Interactive Branch</strong>-  Founded in 2006 by a  group of Imperial College postgraduate students from the Imperial College  Business School and the Faculty of Medicine. The goal is to stimulate interest  in entrepreneurialism among College students and staff, by becoming founder managers  of  start-up businesses and motivate students  to participate in the Entrepreneurs’ Challenge.</li>
-        <li><strong>New worlds financial confererence</strong> - Imperial College, London will be holding its' first ever finance conference at Bloomberg HQ, on <strong>Saturday 5th February 2011</strong>. We are inviting a select 240 members from this society and other leading university societies to attend this conference. In this futuristic environment successful applicants will be able to enjoy presentations of the highest quality by leading professionals and will be presented with a unique opportunity to network and mingle with all our sponsors during the breaks.</li>
-    </ul>
-</div>
-
-<div id="history">
-    <h3>History</h3>
-    <p class=displaytext>The year 1999 marked the launch of ICFS, one of  the largest, most proactive student societies in the country. From around 250  members at that time, we have grown to over 2400 members at present through  pro-actively developing our offering to our members and sponsors. During this  time we have forged strong links to some of the largest and most prestigious  firms in industry. Throughout this time we have received the full backing of  the Imperial College Business School, and we continue to work closely with them.</p>               
-</div> 
+    <h1><?php echo urldecode($row_homeregion['title']); ?></h1>
+<img src="images/welcome.jpg" width="100%">
+<div class="displaytext">
+                      <?php echo stripslashes(urldecode($row_homeregion['body'])); ?>
+</div>                      
 
 <? require_once('footer.php');?>
--- index.php
+++ index.php
@@ -1,46 +1,22 @@
 <?php
-    $currentpage = 'index';
-    require_once('header.php');
-    mysql_select_db($database_sql, $sql);
-    $query_homeregion = "SELECT * FROM homepage WHERE ID = 1";
-    $homeregion = mysql_query($query_homeregion, $sql) or die(mysql_error());
-    $row_homeregion = mysql_fetch_assoc($homeregion);
-    $totalRows_homeregion = mysql_num_rows($homeregion);
+$currentpage = 'index';
+require_once('header.php');
+mysql_select_db($database_sql, $sql);
+
 ?>
 
-<h1><?php echo urldecode($row_homeregion['title']); ?></h1>
-<!-- <img src="images/welcome.jpg" width="100%"> -->
+<h1>Welcome</h1>
+<img src="images/welcome.jpg" width="100%">
+<BR /><BR />
 <div class="displaytext">
-    <?php echo stripslashes(urldecode($row_homeregion['body'])); ?>
+This website is currently under construction, however will be finished shortly,... it is fully functional but please forgive any aesthetic mishaps!
 </div>
-        
-<div class="amplifytradingbox grid_8">
-    <img src="images/pdf_icon.png" width="32" height="32" alt="Download Amplify Trading" />
-    <a href="downloadlatest.php">Download the latest Amplify Trading report </a><?php if(!isset($_SESSION['username'])) { ?>(you must be logged in to do so) <?php } ?>
-    | <a href="amplifytrading.php">View all reports</a>
-</div>
-
-<div class="clear"></div>
-
-<div id="eventsbox" class="grid_6">
-    <h4>Upcoming Events</h4>
-    <ul>
-        <?php 
-        if($totalRows_Recordset1 > 0) {
-            do {
-        ?>
-            <li class="clearfix">
-                <div class="date">
-                    <span class="day"><?php echo date("d",strtotime($row_Recordset1['date'])); ?></span> 
-                    <span class="month"><?php echo date("M",strtotime($row_Recordset1['date'])); ?></span>
-                </div>
-                <div class="title"><?php echo urldecode($row_Recordset1['title']); ?></div>
-            </li>
-            <?php } while ($row_Recordset1 = mysql_fetch_assoc($Recordset1)); ?>
-        <?php } else { ?>
-            <p>No upcoming events.</p>
-        <?php } ?>
-    </ul>
-</div>
+<BR />        
+       
+       <div class=amplifytradingbox>
+       <img src="images/pdf_icon.png" width="32" height="32" alt="Download Amplify Trading" />
+                         <a href="downloadlatest.php">Download the latest Amplify Trading report </a><?php if(!isset($_SESSION['username'])) { ?>(you must be logged in to do so) <?php } ?>
+                         | <a href="amplifytrading.php">View all reports</a>
+        </div>
 
 <?php require_once('footer.php');  ?>
