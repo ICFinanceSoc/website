b4b850174f6a5b503fb7728123d1dc176ed66c55
Jonathan Kim <jkimbo@gmail.com>
2011-11-08 01:41:13 +0000

    Changed subsidiary page layout

--- subsidiaries.php
+++ subsidiaries.php
@@ -11,12 +11,12 @@ $totalRows_homeregion = mysql_num_rows($homeregion);
 <div class="displaytext" id="<?php echo $currentpage; ?>">
     <p class="displaytext">There are several clubs that operate under&nbsp;<acronym title="Imperial College Finance Society">ICFS</acronym>. You can visit their websites to learn more about them.</p>
 
-    <div class="subbox grid_6 alpha">
+    <div class="subbox">
         <h3>Investment Club</h3>
         <div class="imagebox">
             <!-- <img src="http://union.ic.ac.uk/scc/finance/new/images/icfs-ic.gif" alt="" width="180" height="17" /> -->
             <a title="ICFS Investment Club" href="http://www.iciclub.co.uk/" target="_blank">
-                <img src="images/icic.jpg" alt="" width="450"/>
+                <img src="images/icic.jpg" alt="" width="400"/>
             </a>
         </div>
         <p class="displaytext">
@@ -24,31 +24,31 @@ $totalRows_homeregion = mysql_num_rows($homeregion);
         </p>
     </div>
 
-    <div class="subbox grid_6 omega">
-        <h3>Entrepreneurs Interactive</h3>
-        <div class="imagebox">
-            <!-- <img src="http://union.ic.ac.uk/scc/finance/new/images/icfs-ei.gif" alt="Imperial College Finance Society - Entrepreneurs Interactive" /> -->
-            <a title="ICFS Entrepreneurs Interactive" href="http://entrepreneursinteractive.com/icbs/" target="_blank">
-                <img src="images/ei.png" alt="Imperial College Finance Society - Entrepreneurs Interactive" width="450"/>
-            </a>
-        </div>
-        <p class="displaytext">
-            <a title="ICFS Entrepreneurs Interactive" href="http://entrepreneursinteractive.com/icbs/" target="_blank"><strong>Entrepreneurs Interactive</strong></a>&nbsp;was founded in summer 2006 as IC Entrepreneurs by a group of Imperial College postgraduate students. Last year we organised more than 10 events for over 500 attendants. The mission of Entrepreneurs Interactive is to foster entrepreneurship among students, staff and alumni of Imperial and promote entrepreneurial mind-set as a founder or manager of a start-up company. The club hosts events and provides resources to support the finding of innovations and facilitates networking with like-minded people, VCs and the Imperial College entrepreneurial community.
-        </p>
-    </div>
-    
-    <div class="clear"></div>
-
-    <div class="subbox grid_6 alpha">
+    <div class="subbox last">
         <h3>ICFS New Financial World Conference</h3>
         <div class="imagebox">
             <!-- <a href="http://www.icfsconference.com/" target="_blank"><img src="http://union.ic.ac.uk/scc/finance/new/images/nfwclogo.jpg" alt="" width="225" height="50" /></a> -->
             <a href="http://www.icfsconference.com/" target="_blank">
-                <img src="images/nfw.jpg" alt="ICFS New Financial World Conference" width="450"/>
+                <img src="images/nfw.jpg" alt="ICFS New Financial World Conference" width="400"/>
             </a>
         </div>
         <p class="displaytext">
             <strong><a href="http://www.icfsconference.com/">New worlds financial confererence</a></strong>&nbsp;- Imperial College, London will be holding its' first ever finance conference at&nbsp;Bloomberg HQ, on<strong>&nbsp;5th February 2012</strong>. We are inviting a select 240 members from this society and other leading university societies to attend this conference. In this futuristic environment successful applicants will be able to enjoy presentations of the highest quality by leading professionals and will be presented with a unique opportunity to network and mingle with all our sponsors during the breaks.
+        </p>
+    </div>
+
+    <div class="clear"></div>
+
+    <div class="subbox last">
+        <h3>Entrepreneurs Interactive</h3>
+        <div class="imagebox">
+            <!-- <img src="http://union.ic.ac.uk/scc/finance/new/images/icfs-ei.gif" alt="Imperial College Finance Society - Entrepreneurs Interactive" /> -->
+            <a title="ICFS Entrepreneurs Interactive" href="http://entrepreneursinteractive.com/icbs/" target="_blank">
+                <img src="images/ei.png" alt="Imperial College Finance Society - Entrepreneurs Interactive" width="400"/>
+            </a>
+        </div>
+        <p class="displaytext">
+            <a title="ICFS Entrepreneurs Interactive" href="http://entrepreneursinteractive.com/icbs/" target="_blank"><strong>Entrepreneurs Interactive</strong></a>&nbsp;was founded in summer 2006 as IC Entrepreneurs by a group of Imperial College postgraduate students. Last year we organised more than 10 events for over 500 attendants. The mission of Entrepreneurs Interactive is to foster entrepreneurship among students, staff and alumni of Imperial and promote entrepreneurial mind-set as a founder or manager of a start-up company. The club hosts events and provides resources to support the finding of innovations and facilitates networking with like-minded people, VCs and the Imperial College entrepreneurial community.
         </p>
     </div>
 
