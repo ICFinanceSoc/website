9efa7a0a16a2b5483d937ae113679f5ded2d0f9a
Thomas Lim <txl11@dougal.union.ic.ac.uk>
2012-08-30 02:11:50 +0100

    Changed db.php. No longer calling db.local.php to avoid any issues. All done via defines now

--- db.php
+++ db.php
@@ -1,19 +1,33 @@
 <?
 $cwd = getcwd();
-$cwdlength = strlen($cwd);
+/*$cwdlength = strlen($cwd);
 if (substr($cwd, -5) === 'admin') {
 	$wroot = substr_replace($cwd, '', -6);
 }
 else{
 	$wroot = $cwd;
-}
+}*/
 
-if(file_exists($wroot.'/db.local.php')) {
+/*if(file_exists($wroot.'/db.local.php')) {
     require_once('db.local.php');
-} else {
+} else {*/
+define('location', 'dev');
+
+if(location == 'dev'){
+    $dbname = 'scc_finance_dev';
+    $user = 'scc_finance';
+    $pass = 'phqxNcL6BLSfLBjr';
+    define('HOME_PAGE', 'https://www.union.ic.ac.uk/scc/finance/dev/');
+} elseif(location == 'live'){
     $dbname = 'scc_finance';
     $user = 'scc_finance';
     $pass = 'phqxNcL6BLSfLBjr';
+    define('HOME_PAGE', 'https://www.union.ic.ac.uk/scc/finance/');
+} elseif(location == 'local'){
+	$dbname = 'scc_finance_dev';
+    $user = 'scc_finance';
+    $pass = 'password';
+    define('HOME_PAGE', 'http://localhost/finance/');
 }
 
 $connect = mysql_connect('localhost', $user, $pass);
@@ -24,9 +38,9 @@ if (!$connect) {
 mysql_select_db($dbname, $connect);
 
 /* turn off error reporting */
-error_reporting(0);
+//error_reporting(0);
 /* to turn on error reporting uncomment line: */
-//error_reporting(E_ERROR | E_WARNING | E_PARSE);
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
 if(!defined('HOME_PAGE')) define('HOME_PAGE', 'https://www.union.ic.ac.uk/scc/finance/');
 if(!defined('LOCAL')) define('LOCAL', false);
