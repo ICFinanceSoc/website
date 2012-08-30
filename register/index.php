c5acebe8da50726b9c2815d7d65e2d075316315e
Thomas Lim <txl11@ic.ac.uk>
2012-08-30 01:45:18 +0100

    New registration form added

---
+++ register/index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE8" />
+    <title>Imperial College Finance Society</title>
+
+    <!-- Main stylesheet -->
+    <!-- <link rel="stylesheet" type="text/css" href="CSS/style.css" /> -->
+    <link rel="stylesheet/less" type="text/css" href="../CSS/style.less" />
+    <script src="../js/less-1.1.4.min.js" type="text/javascript"></script>
+</head>
+
+<h3>Website, Event and Mailing list Registration</h3>
+<?
+require_once('../db.php');
+    $show = 0;
+    $message = 0;
+
+    $submit = $_GET['submit'];
+    $Interests = ',';
+    if($submit == 1){
+        echo 'submitted';
+        $Username = mysql_real_escape_string($_POST['Username']);
+        $Mobile = mysql_real_escape_string($_POST['Mobile']);
+        $Interests1 = $_POST['Interests'];
+        include('/usr/local/share/union-php/_auto_pre.php');
+        if ($Interests1){
+            foreach ($Interests1 as $I){
+                $Interests = $Interests.$I.',';
+            }
+        }
+        $filter= '(uid=';
+        $filter .= $Username;
+        $filter .= ')';
+        $conn = ldap_connect("addressbook.ic.ac.uk") or die("Could not connect to server");
+        $r = ldap_bind($conn) or die("Could not bind to server");
+        $dn = "o=Imperial College,c=GB";
+        $justthese = array("ou");
+        $sr=ldap_search($conn, $dn, $filter, $justthese);
+        $info = ldap_get_entries($conn, $sr);
+        $Dept = $info[0]["ou"][0];
+        ldap_close($conn);
+        //$validpassword = pam_auth($Username, $Password);
+        $names = ldap_get_names($Username);
+        $name = $names[0];
+        if($name){
+            if(mysql_num_rows(mysql_query("SELECT * FROM 2012_Business_school WHERE Username = '$Username'"))==0) {
+                mysql_query("INSERT INTO 2012_Business_school (Username, Mobile, Interests,Dept)
+                VALUES ('$Username', '$Mobile', '$Interests', '$Dept')");
+                $message = 1;
+                $show = 1;
+            } else {
+                $message = 2;
+            }
+        } else {
+            $message = 3;
+        }
+    }
+
+    if($message == 0){ ?>
+    <p class="info">Please enter your details in order to sign up to our society.</p>
+    <? }
+
+    if($message == 1){ ?>
+    <p class="success">Thank you. You have succesfully confirmed your identity and are now on the Finance Society mailing list. Please <a href="requirelogin.php">now log in</a>.</p>
+    <? }
+
+    if($message == 2){ ?>
+    <p class="error">Something went wrong. It would appear that you are already a member of the society.</p>
+    <? }
+
+    if($message == 3){ ?>
+    <p class="error">The username supplied does not exist.</p>
+    <? }
+
+//if($show == 0){ ?>
+        <form action="?submit=1" method="POST">
+            <fieldset>
+                <div class="clearfix">
+                    <label for="Username">College Username</label>
+                    <div class="input">
+                        <input type="text" name="Username" id="Username" />
+                    </div>
+                </div>
+
+                <div class="clearfix">
+                    <label for="Mobile">Mobile Number</label>
+                    <div class="input">
+                        <input type="text" name="Mobile" id="Mobile" />
+                    </div>
+                </div>
+                <div class="clearfix">
+                    <label for="Interests">Interests</label>
+                    <div class="input">
+                        <select name="Interests[]" multiple="multiple">
+                        <?
+                            $LookupInterests = mysql_query("SELECT * FROM 2011_Interests");
+                            $num=mysql_numrows($LookupInterests);
+                            $i=0;
+                            while ($i < $num) {
+                                $field1=mysql_result($LookupInterests,$i,"Interest_ID");
+                                $field2=mysql_result($LookupInterests,$i,"InterestName");
+                                echo "<option value=\"$field1\" name=\"Interests[]\">$field2</option>";
+                                $i++;
+                            }
+                        ?>
+                        </select>
+                        <span class="help-block">Feel free to select multiple options - hold down Ctrl/Cmd</span>
+                    </div>
+                </div>
+                <div class="actions">
+                    <input type="submit" name="submit" class="btn primary" />
+                </div>
+            </fieldset>
+        </form>
+        <h4>Why do we need your username?</h4>
+        <p>The ICFS website now runs on the College SSO authentication system. We require it now to confirm your identity.</p>
+        <h4>Does this make me a society member?</h4>
+        <p>Not exactly. We encourage you to 'buy' free membership of the ICFS on the union website, in order to register officially with the union as a member of the society. This will allow you to vote in ICFS elections. This registration system is our communications system: registering on this website will ensure you receive updates on events we run and publications we release.</p>
+    <? //} ?>
