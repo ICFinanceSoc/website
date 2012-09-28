52d50123e832b4258edb424ffb9771568851e045
Thomas Lim <txl11@dougal.union.ic.ac.uk>
2012-09-28 09:47:49 +0100

    Renamed management page to team

---
+++ team.php
@@ -0,0 +1,38 @@
+<?php 
+$currentpage = 'management'; 
+require_once('header.php');
+
+mysql_select_db($database_sql, $sql);
+$query_Recordset1 = "SELECT * FROM management ORDER BY `order` ASC";
+$Recordset1 = mysql_query($query_Recordset1, $sql) or die(mysql_error());
+$row_Recordset1 = mysql_fetch_assoc($Recordset1);
+$totalRows_Recordset1 = mysql_num_rows($Recordset1);
+?>
+
+<div id="<?php echo $currentpage; ?>">
+<?php do { ?>
+    <?php $currentimage = $row_Recordset1['image']; ?>
+    <div class="person clearfix <?php if(file_exists("managementpictures/$currentimage")) echo 'imageactive'; ?>">
+        <div class="image">
+        <?php
+            if(file_exists("managementpictures/$currentimage")) { ?>
+                <img src="managementpictures/<?php echo $row_Recordset1['image']; ?>" alt="" width="200" />
+        <?php } ?>
+        </div>
+        <div class="leftpointer"></div>
+        <div class="personinfo">
+            <div class="name">
+                <h2><?php echo urldecode($row_Recordset1['name']); ?></h2>
+            </div>
+            <div class="info">
+                <div id="position">
+                    <p><?php echo urldecode($row_Recordset1['position']); ?> - <?php echo urldecode($row_Recordset1['degree']); ?></p>
+                </div>
+                <p><?php echo urldecode($row_Recordset1['blurb']); ?></p>
+            </div>
+        </div>
+    </div>
+<?php } while ($row_Recordset1 = mysql_fetch_assoc($Recordset1)); ?>
+</div>
+                       
+<?php require_once('footer.php');  ?>
