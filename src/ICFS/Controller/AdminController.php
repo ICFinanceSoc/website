ca490f77dfe2e7bebbfd95b1070dc79a5818b175
Dario Magliocchetti <dario_dml@hotmail.com>
2013-02-26 23:41:40 +0000

    Completed POST for ICFS Team management.

--- src/ICFS/Controller/AdminController.php
+++ src/ICFS/Controller/AdminController.php
@@ -320,23 +320,21 @@ class AdminController implements ControllerProviderInterface
 
         $this->controllers->get('team/', function (Application $app) {
             return $app->redirect($app['url_generator']->generate('ngap', array(), true) . 'team/' . (date('Y') - (date('m') < 8)));
-
         })->before($this->allowed())->before($this->nav->fetch());
 
         $this->controllers->get('team/{year}', function (Application $app, $year) {
             $team = new \ICFS\Model\ICFSTeam($app);
             $comittee = $team->getComittee($year);
+
             return $app['twig']->render('ngap/icfsteam', array('selectyear' => $year, 'comittee' => $comittee));
         })->before($this->allowed())->before($this->nav->fetch());
 
         $this->controllers->post('team/{year}', function (Application $app, $year) {
-
-            var_dump($app['request']->request->all());
-
             $team = new \ICFS\Model\ICFSTeam($app);
 
-            return $app['twig']->render('ngap/icfsteam', array('selectyear' => $year, 'comittee' => $app['request']->request->get('comittee')));
-
+            if ($error = $team->updateFromPost($year))
+                return $app['twig']->render('ngap/icfsteam', array('selectyear' => $year, 'comittee' => $app['request']->request->get('comittee')));
+            return $app->redirect($app['url_generator']->generate('ngap', array(), true) . 'team/' . $year);
         })->before($this->allowed())->before($this->nav->fetch());
     }
 }
