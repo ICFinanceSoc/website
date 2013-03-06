78b95430f93c26fc84d96d69566e3764d4c90cfa
Thomas Lim <txl11@ic.ac.uk>
2013-03-06 07:06:01 +0000

    AGM registration started. Form looks ok

---
+++ agm/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>ICFS AGM 2013</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+        <link rel="icon" href="../favicon.ico">
+
+        <link rel="stylesheet" href="bs/css/bootstrap.min.css">
+        <link rel="stylesheet" href="bs/css/bootstrap-responsive.min.css">
+        <link rel="stylesheet" href="agm.css">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="span12 pagination-centered logo-top"><img src="../images/logo.png" style alt="Imperial College Finance Society"></div>
+            </div>
+
+            <div class="row-fluid">
+                <div class="span12 pagination-centered title-top"><h1>AGM 2013 Registration</h1></div>
+            </div>
+
+            <div class="row-fluid">
+                <div class="span12 pagination-centered title-top" style="margin-left:7px"><h3>Welcome to the ICFS 2013 AGM. Please sign in to register your attendance, and to receive your electronic ballot paper.</h3></div>
+            </div>
+
+            <div class="row-fluid hidden-desktop">
+                <div class="span12 offset2 form-top">
+                        <form class="form-horizontal">
+                          <div class="control-group">
+                            <label class="control-label" for="user">College Username</label>
+                            <div class="controls">
+                              <input type="text" id="user" placeholder="Username">
+                            </div>
+                          </div>
+                          <div class="control-group">
+                            <label class="control-label" for="pass">Password</label>
+                            <div class="controls">
+                              <input type="password" id="pass" placeholder="Password">
+                            </div>
+                          </div>
+                          <div class="control-group">
+                            <div class="controls">
+                              <button type="submit" class="btn">Sign in</button>
+                            </div>
+                          </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="row-fluid visible-desktop">
+                    <div class="span12 offset4 form-top">
+                            <form class="form-horizontal">
+                              <div class="control-group">
+                                <label class="control-label" for="user">College Username</label>
+                                <div class="controls">
+                                  <input type="text" id="user" placeholder="Username">
+                                </div>
+                              </div>
+                              <div class="control-group">
+                                <label class="control-label" for="pass">Password</label>
+                                <div class="controls">
+                                  <input type="password" id="pass" placeholder="Password">
+                                </div>
+                              </div>
+                              <div class="control-group">
+                                <div class="controls">
+                                  <button type="submit" class="btn">Sign in</button>
+                                </div>
+                              </div>
+                            </form>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </body>
+</html>
